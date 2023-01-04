1ddc6de5f302b3c51619e3d6f46164f340911299
François TOURDE <fr-github@tourde.org>
2023-01-04 17:23:47 +0100

    fix: Some changes on script behavior and shell run
    * Add stop option on script errors
    * Reload daemon when services changed

--- templates/kiosk.sh
+++ templates/kiosk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -eu
+
 unclutter -idle 0.5 -root &
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/{{ jukebox__default_user }}/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/{{ jukebox__default_user }}/.config/chromium/Default/Preferences
